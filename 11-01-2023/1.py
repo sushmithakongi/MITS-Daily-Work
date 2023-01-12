13ab61d1a86ec3981694db3b9fbdbc437fc9edee
tejavardhan <crreddy@infobellit.com>
2023-01-12 10:52:39 +0530

    codes

--- 11-01-2023/1.py
+++ 11-01-2023/1.py
@@ -1,1 +1,25 @@
-print("hello")
+'''You are required to implement the following function:
+Int SumOddIntegers(int arr[], int n);
+The function accepts an integer array ‘arr’ of length ‘n’ as its argument. You are required to 
+calculate the sum of all odd integers in an array ‘an’ and return the same.
+Note:
+Array can have negative integers
+n > 0
+Computed values lie within integer range
+Example:
+Input:
+arr: 1 4 6 7 10 12 11 5
+n: 8
+Output:
+24
+Explanation:
+The odd integers in array {1, 4, 6, 7, 10, 12, 11, 5} are {1, 7,11, 5} and their sum is 24, hence 24 is 
+'''
+
+a=list(map(int,input().split()))
+sum=0
+for i in a:
+    if i%2!=0:
+        
+        sum=sum+i
+print(sum)
