ff0facf0200c3279a93138eb2213a9b4cc41fccf
HarshithaVenkat <121852184+HarshithaVenkat@users.noreply.github.com>
2023-02-08 11:26:16 +0530

    Pyhton code 2 on 08/02/23

---
+++ Sort Characters by Frequency.py
@@ -0,0 +1,31 @@
+""" Sort Characters By Frequency
+Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
+Return the sorted string. If there are multiple answers, return any of them.
+Example 1:
+Input: s = "tree"
+Output: "eert"
+Explanation: 'e' appears twice while 'r' and 't' both appear once.
+So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+Example 2:
+Input: s = "cccaaa"
+Output: "aaaccc"
+Explanation: Both 'c' and 'a' appear three times, so both "cccaaa" and "aaaccc" are valid answers.
+Note that "cacaca" is incorrect, as the same characters must be together.
+Example 3:
+Input: s = "Aabb"
+Output: "bbAa"
+Explanation: "bbaA" is also a valid answer, but "Aabb" is incorrect.
+Note that 'A' and 'a' are treated as two different characters."""
+
+def frequencySort(s):
+        l=[]
+        for i in set(s):
+            l.append([i,s.count(i)])
+        u=sorted(l, key = lambda x: x[1])
+        res=""
+        for i in u[::-1]:
+            res+=i[0]*i[1]
+        return res
+s = "tree"
+a=frequencySort(s)
+print(a)
