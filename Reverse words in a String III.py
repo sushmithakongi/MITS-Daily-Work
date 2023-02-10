0598a16ecadb6880f64126b37e9d409ba28a339b
HarshithaVenkat <121852184+HarshithaVenkat@users.noreply.github.com>
2023-02-10 14:56:22 +0530

    Python code 1 on 10/02/23

---
+++ Reverse words in a String III.py
@@ -0,0 +1,17 @@
+"""Reverse Words in a String III
+Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+Example 1:
+Input: s = "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+Example 2:
+Input: s = "God Ding"
+Output: "doG gniD" """
+def reverseWords(s):
+        x=s.split()
+        l=[]
+        for i in x:
+            l.append(i[::-1])
+        return ' '.join(l)
+s = "Let's take LeetCode contest"
+a=reverseWords(s)
+print(a)
