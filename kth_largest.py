c8592dc250f6f0bd2f1d103ad39e0e1fc8dd0316
HarshithaVenkat <121852184+HarshithaVenkat@users.noreply.github.com>
2023-02-27 16:13:01 +0530

    Python code 2 on 27-02-23

---
+++ kth_largest.py
@@ -0,0 +1,21 @@
+""" Kth Largest Element in an Array
+Given an integer array nums and an integer k, return the kth largest element in the array.
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+You must solve it in O(n) time complexity.
+Example 1:
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+Example 2:
+Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+Output: 4"""
+
+
+def findKthLargest(nums, k):
+    nums.sort()
+    return nums[-k]
+
+
+nums = [3, 2, 3, 1, 2, 4, 5, 5, 6]
+k = 4
+a = findKthLargest(nums, k)
+print(a)
